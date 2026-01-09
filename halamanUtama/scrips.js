773ab849e5b578193b760ff3ad2f11212a461387
dimazfrezna <alfarizdzaki787@gmail.com>
2026-01-09 21:16:46 +0700

    delete file
    delete

--- halamanUtama/scrips.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("lihatSelengkapnya").addEventListener("click", function () {
-    document.getElementById("galeri").scrollIntoView({ behavior: "smooth" });
-});
-
-document.getElementById('menu-button').addEventListener('click', function () {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('hidden');
-});
